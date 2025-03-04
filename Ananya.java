28644869cfc7fa20015b8ae12ddbdef4d009b42a
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-04 18:39:20 +0530

    String in Java

---
+++ Ananya.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Ananya{
+    public static void main(String args[]){
+
+        String a= "Ananya";
+        System.out.println(a);
+
+        String obj=new String("Garima");
+        System.out.println(obj);
+
+        char arr[]={'A','a','s','t','h','a'};
+        System.out.println(arr);
+
+        String obj1=new String(arr);
+        System.out.println(obj1);
+
+
+
+
+
+    }
+}
