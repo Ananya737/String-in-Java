072deddb197ce31a27dce1144e8d5cfe6719e7a9
Ananya Sahu <ananyasahu59513@gmail.com>
2025-03-25 14:59:47 +0530

    update

--- Subsequence.java
+++ Subsequence.java
@@ -1,34 +1,53 @@
-import java.util.Scanner;
-public class Subsequence{
+// import java.util.Scanner;
+// public class Subsequence{
 
-    static boolean subseq(String str1,String str2){
-        int i=0;
-        int j=0;
-        while(i<str1.length() && j<str2.length()){
-            if(str1.charAt(i)==str2.charAt(j)){
-                i++;
-            }
+//     static boolean subseq(String str1,String str2){
+//         int i=0;
+//         int j=0;
+//         while(i<str1.length() && j<str2.length()){
+//             if(str1.charAt(i)==str2.charAt(j)){
+//                 i++;
+//             }
             
-                j++;
+//                 j++;
            
-        }
+//         }
 
-        return i==str1.length();
+//         return i==str1.length();
 
-    }
+//     }
 
-    public static void main(String args[]){
-        String str1="abc";
-        String str2="abcd";
-        if(subseq(str1,str2)){
-            System.out.println("Yes");
-        }
-        else{
-            System.out.println("No");
-        }
+//     public static void main(String args[]){
+//         String str1="abc";
+//         String str2="abcd";
+//         if(subseq(str1,str2)){
+//             System.out.println("Yes");
+//         }
+//         else{
+//             System.out.println("No");
+//         }
     
-    }
-}
+//     }
+// }
 
 
   
+
+
+
+
+
+  public class Subsequence{
+    public static void main(String args[]){
+        String str1="abcd";
+        String str2="acb";
+        int count=0;
+        int j=0;
+        for(int i=0;i<str1.length();i++){
+            if(str1.charAt(i)==str2.charAt(j)){
+                j++;
+            }
+        }
+        System.out.println(j);
+    }
+  }
